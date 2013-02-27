ae475b265728c314129c86ee018bc1fe1b3e3ef9
Gorath <generalredeye@gmail.com>
2013-02-27 11:46:26 +0000

    Finish Step 2

--- cw2/jadeCW/src/jadeCW/PatientAgent.java
+++ cw2/jadeCW/src/jadeCW/PatientAgent.java
@@ -1,6 +1,15 @@
 package jadeCW;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.SearchConstraints;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.proto.SubscriptionInitiator;
+import jade.util.leap.Iterator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,27 +23,85 @@ import java.util.List;
  */
 public class PatientAgent extends Agent {
 
-    protected void setup() {
 
+    AID appointmentAllocatorProvider;
+
+    protected void setup() {
+        String inputString = System.console().readLine().trim();
+        parsePatientAppointmentPreferences(inputString);
+        subscribeToDF();
+    }
+
+
+    private void subscribeToDF() {
+        // Build the description used as template for the subscription
+        DFAgentDescription template = new DFAgentDescription();
+        ServiceDescription templateSd = new ServiceDescription();
+        templateSd.setType("allocate-appointments");
+        template.addServices(templateSd);
+
+        SearchConstraints sc = new SearchConstraints();
+        // We want to receive 10 results at most
+        sc.setMaxResults(new Long(10));
+
+        addBehaviour(new SubscriptionInitiator(this, DFService.createSubscriptionMessage(this, getDefaultDF(), template, sc)) {
+            protected void handleInform(ACLMessage inform) {
+                System.out.println("Agent "+getLocalName()+": Notification received from DF");
+                try {
+                    DFAgentDescription[] results = DFService.decodeNotification(inform.getContent());
+                    if (results.length > 0) {
+                        for (int i = 0; i < results.length; ++i) {
+                            DFAgentDescription dfd = results[i];
+                            AID provider = dfd.getName();
+                            // The same agent may provide several services; we are only interested
+                            // in the weather-forcast one
+                            Iterator it = dfd.getAllServices();
+                            while (it.hasNext()) {
+                                ServiceDescription sd = (ServiceDescription) it.next();
+                                if (sd.getType().equals("allocate-appointments") && appointmentAllocatorProvider == null) {
+                                    System.out.println("Appointment allocator found");
+                                    System.out.println("- Service \""+sd.getName()+"\" provided by agent "+provider.getName());
+                                    // Stores the allocator provider agent for later use
+                                    appointmentAllocatorProvider = provider;
+                                }
+                            }
+                        }
+                    }
+                    System.out.println();
+                }
+                catch (FIPAException fe) {
+                    fe.printStackTrace();
+                }
+            }
+        } );
+    }
+
+    public List<ArrayList<Integer>> parsePatientAppointmentPreferences(String line) {
         List<ArrayList<Integer>> appointmentPreferences = new ArrayList<ArrayList<Integer>>();
 
-        String inputString = System.console().readLine().trim();
+        String[] splitString = line.split("-");
 
-        String[] splitString = inputString.split("-");
+        // If line is "-" then add a blank array list for consistency
+        if (splitString.length == 0) {
+            appointmentPreferences.add(new ArrayList<Integer>());
+        }
 
+        // For each sub-section of preferences add a new array list
         for (int i = 0; i < splitString.length; i++) {
             String[] splitPrefRow = splitString[i].split(" ");
             ArrayList<Integer> row = new ArrayList<Integer>();
+
+            // Add each number in this preference depth to the ArrayList
             for (int j = 0; j < splitPrefRow.length; j++) {
-                if (!splitPrefRow[j].equals(" ") || !splitPrefRow[j].equals("")) {
+                if (!splitPrefRow[j].equals(" ") && !splitPrefRow[j].equals("")) {
                     row.add(Integer.parseInt(splitPrefRow[j]));
                 }
             }
+
             appointmentPreferences.add(row);
         }
 
-        doDelete();
-
+        return appointmentPreferences;
     }
 
 }
---
+++ cw2/jadeCW/src/jadeCW/RequestAppointment.java
@@ -0,0 +1,23 @@
+package jadeCW;
+
+import jade.core.behaviours.Behaviour;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gdj08
+ * Date: 27/02/13
+ * Time: 11:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class RequestAppointment extends Behaviour {
+
+    @Override
+    public void action() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public boolean done() {
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
