66c302e3740b71f70fdc0de195faf78bac07c3cd
Gorath <generalredeye@gmail.com>
2013-03-05 10:20:47 +0000

    Update ignore

---
+++ cw2/jadeCW/testingStuff/testingSplitString.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: gdj08
+ * Date: 27/02/13
+ * Time: 11:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class testingSplitString {
+}
